ab11aacbabbf7e369f5dd98f77b010e22cb60e19
swellwheel <b09207002@ntu.edu.tw>
2024-10-14 19:14:18 +0800

    test10142

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloaddddddddddd.
+          Edit <code>src/App.js</code> and save to reloadddddd.
         </p>
         <a
           className="App-link"
