81f59c8960f2f781bb46f9b2b23df1432412c728
root <root@DESKTOP-VOMOL21.>
2024-11-10 20:07:26 +0800

    refactor: clean homepage clapy code & reset framework

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,16 +2,18 @@ import { memo } from 'react';
 import type { FC } from 'react';
 
 import classes from './App.module.css';
-import resets from './components/_resets.module.css';
-import { Homepage } from './components/Homepage/Homepage.js';
+import logo from '/assets/logo.png';
 
 interface Props {
   className?: string;
 }
 export const App: FC<Props> = memo(function App(props = {}) {
   return (
-    <div className={`${resets.clapyResets} ${classes.root}`}>
-      <Homepage />
+    <div 
+      className={`${classes.root}`}>
+    <img
+        className={classes.logo}
+        src={logo}/>    
     </div>
   );
 });
