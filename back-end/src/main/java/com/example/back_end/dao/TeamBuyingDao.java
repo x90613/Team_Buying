47a3b16995f8eb4403883094c47e95b74cf373b0
R13725056 <R13725056@ntu.edu.tw>
2024-12-04 22:54:20 +0800

    add frontend and backend docker file

--- back-end/src/main/java/com/example/back_end/dao/TeamBuyingDao.java
+++ back-end/src/main/java/com/example/back_end/dao/TeamBuyingDao.java
@@ -18,7 +18,7 @@ public class TeamBuyingDao {
     String sql =
         "SELECT hf.id, hf.host_id, hf.dead_time, hf.start_time, hf.description, hf.menu_id,"
             + " hf.open, hf.status, hf.title, hf.image, hf.store_name, hf.transfer_information,"
-            + " hf.host_contact_information, (SELECT COUNT(*) FROM Participant_form pf WHERE"
+            + " hf.host_contact_information, (SELECT COUNT(*) FROM participant_form pf WHERE"
             + " pf.host_form_id = hf.id) AS participantCount, (SELECT COALESCE(AVG(uf.score), 0)"
             + " FROM user_feed_back uf WHERE uf.host_id = hf.host_id) AS averageFeedbackScore,"
             + " m.name AS menu_store_name, m.img as menu_store_img, u.username As user_name FROM"
